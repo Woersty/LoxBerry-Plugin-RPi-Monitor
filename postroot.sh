20fbdcfd7a6517a3d6ae3cc02a5b696f63c49681
Wörsty <git@loxberry.woerstenfeld.de>
2018-03-04 12:23:58 +0100

    v2018.3.4
    -New Version for LoxBerry 1.0.3+

---
+++ postroot.sh
@@ -0,0 +1,24 @@
+echo "Installing/Reinstalling RPiMonitor package"
+systemctl stop rpimonitor >/dev/null 2>&1
+dpkg -i REPLACELBPDATADIR/rpimonitor_2.12-r0_all.deb 
+set -x
+cp REPLACELBPDATADIR/*.conf /etc/rpimonitor/template/
+if [ -d "/tmp/rpi.save/" ]; then
+	cp -f /tmp/rpi.save/*.conf /etc/rpimonitor/template/ 
+fi
+mkdir -p /tmp/rpi.save/
+cp -f /etc/rpimonitor/template/*.conf /tmp/rpi.save/ 
+rm -rf REPLACELBPDATADIR 
+ln -s /etc/rpimonitor/template REPLACELBPDATADIR
+cp -f /tmp/rpi.save/*.conf /etc/rpimonitor/template/ 
+rm -rf /tmp/rpi.save >/dev/null 2>&1
+chmod -R 755 REPLACELBPDATADIR
+chown -R loxberry REPLACELBPDATADIR
+chgrp -R loxberry REPLACELBPDATADIR
+chmod -R 755 /etc/rpimonitor/template
+chown -R loxberry /etc/rpimonitor/template
+chgrp -R loxberry /etc/rpimonitor/template
+set +x
+/etc/init.d/rpimonitor update 
+systemctl restart rpimonitor 
+exit 0
